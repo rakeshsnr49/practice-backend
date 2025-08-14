a5408b418a9720da5c2d3bec19212a8f154a8f50
rakeshsnr49 <nidbiz108@gmail.com>
2025-08-14 11:50:06 +0530

    Update usercontroller.js

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -1,11 +1,2 @@
-const User = require('../models/usermodel');
 
-exports.getUsers = async (req, res) => {
-  try {
-    const users = awaituser.find();
-    res.json(users);
-  } catch (err) {res.status(500).json({ error:err.message });
-  res.json({ message: "User list fetched successfully!" });
-                }
-};
 
