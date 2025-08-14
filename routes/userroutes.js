74889e22ebef00f3d6afe7fa20aa56dc1ba91faf
rakeshsnr49 <nidbiz108@gmail.com>
2025-08-14 13:03:57 +0530

    Update userroutes.js

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,7 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const { getUsers } = require('../controllers/userController');
 
-router.get('/', getUsers);
-
-module.exports = router;
